399e9eb57cdd62e668b76f543b8ab42605754cce
Chia Yong Kang <chiayongkang@hotmail.com>
2023-12-05 20:35:24 +0800

    created page for createDataListings

--- app/dashboard/source-data/page.tsx
+++ app/dashboard/source-data/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { contractAddresses, DataListingFactoryAbi } from "@/contracts"
 import { motion } from "framer-motion"
-import { useNetwork, usePrepareContractWrite } from "wagmi"
+import { useContractWrite, useNetwork, usePrepareContractWrite } from "wagmi"
 
 import { FADE_DOWN_ANIMATION_VARIANTS } from "@/config/design"
 import { IsWalletConnected } from "@/components/shared/is-wallet-connected"
@@ -60,13 +60,7 @@ export default function PageSourceData() {
     },
   })
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    // Call the write function using the wagmi hook
-    // You'll need to use the `useContractWrite` hook alongside `usePrepareContractWrite`
-    // to actually send the transaction.
-  }
+  const { data, isLoading, isSuccess, write } = useContractWrite(config)
 
   return (
     <motion.div
@@ -86,37 +80,89 @@ export default function PageSourceData() {
               </span>
             </h3>
             <span className="font-light">
-              <div className="mt-4"></div>
+              <div className="my-4"></div>
             </span>
           </div>
-          <form onSubmit={handleSubmit} className="w-full max-w-xs">
-            <select
-              className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              onChange={(e) => setDataSource(e.target.value)}
-              value={dataSource}
-            >
-              {/* Populate this select with options based on the data providers available */}
-            </select>
-            <input
-              type="number"
-              placeholder="Total number of listings"
-              className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              onChange={(e) => setNumListings(e.target.value)}
-              value={numListings}
-            />
-            <input
-              type="text"
-              placeholder="Total price in WETH"
-              className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              onChange={(e) => setTotalPrice(e.target.value)}
-              value={totalPrice}
-            />
-            <button
-              type="submit"
-              className="mt-4 w-full rounded-md bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700 focus:bg-indigo-700 focus:outline-none"
-            >
-              Create Data Listing
-            </button>
+
+          <form className="w-full max-w-xs space-y-4">
+            <div className="mb-4">
+              <label
+                htmlFor="data-provider"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Select data provider
+              </label>
+              <select
+                id="data-provider"
+                name="data-provider"
+                className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                onChange={(e) => setDataSource(e.target.value)}
+                value={dataSource}
+              >
+                <option value="google-fit" className="flex items-center">
+                  {/* Inline SVG or component for Google Fit logo */}
+                  <img
+                    src="path-to-google-fit-logo"
+                    alt="Google Fit"
+                    className="mr-2 h-4 w-4"
+                  />
+                  Google Fit
+                </option>
+                <option value="spotify" className="flex items-center" disabled>
+                  {/* Inline SVG or component for Spotify logo */}
+                  <img
+                    src="path-to-spotify-logo"
+                    alt="Spotify"
+                    className="mr-2 h-4 w-4"
+                  />
+                  Spotify
+                </option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="num-listings"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Total number of listings
+              </label>
+              <input
+                id="num-listings"
+                type="number"
+                placeholder="Total number of listings"
+                className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                onChange={(e) => setNumListings(e.target.value)}
+                value={numListings}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="total-price"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Total price in WETH
+              </label>
+              <input
+                id="total-price"
+                type="text"
+                placeholder="Total price in WETH"
+                className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                onChange={(e) => setTotalPrice(e.target.value)}
+                value={totalPrice}
+              />
+            </div>
+            <div className="mb-4">
+              <button
+                disabled={!write}
+                onClick={() => write?.()}
+                type="submit"
+                className="w-full rounded-md bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700 focus:bg-indigo-700 focus:outline-none"
+              >
+                Create Data Listing
+              </button>
+            </div>
           </form>
         </div>
         <SuccessModal
@@ -137,3 +183,9 @@ export default function PageSourceData() {
     </motion.div>
   )
 }
+
+// TODO
+// 1. Check if the parameters are correct
+// 2. Obtain the parameters
+// 3. Test creation of data listing
+// 4. Better Styling / Error handling
